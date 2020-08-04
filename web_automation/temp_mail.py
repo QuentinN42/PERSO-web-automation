61db356529efeb33a1f143016967fa201e5a7238
QuentinN42 <quentin.lieumont@gmail.com>
2020-08-05 00:27:06 +0200

    perf(TempMail): remove sleep
    No sleep, just check continuously.

--- web_automation/temp_mail.py
+++ web_automation/temp_mail.py
@@ -2,10 +2,9 @@
 Temp mail
 https://temp-mail.org/en/
 """
-from time import sleep
 
 from .driver import Driver
-from selenium.webdriver.support.ui import WebDriverWait
+from .functions import is_email
 
 
 class TempMail(Driver):
@@ -13,13 +12,11 @@ class TempMail(Driver):
     base_url = "https://temp-mail.org/en/"
     
     def get_mail(self) -> str:
-        while (mail := self.driver.find_element_by_id("mail").get_property("value")) in ["", "Loading ..."]:
+        while not is_email(mail := self.driver.find_element_by_id("mail").get_property("value")):
             pass
         return mail
     
     def main(self) -> None:
-        # print(self.get_mail())
-        sleep(5)
         print(self.get_mail())
         
         input()

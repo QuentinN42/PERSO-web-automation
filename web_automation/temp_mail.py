2e3f02e3ded2fb3623cf25b4b76ffbb21d6ebe9d
QuentinN42 <quentin.lieumont@gmail.com>
2020-08-05 00:08:33 +0200

    feature(TempMail): get_mail
    get_mail method working

--- web_automation/temp_mail.py
+++ web_automation/temp_mail.py
@@ -2,12 +2,24 @@
 Temp mail
 https://temp-mail.org/en/
 """
+from time import sleep
+
 from .driver import Driver
+from selenium.webdriver.support.ui import WebDriverWait
 
 
 class TempMail(Driver):
     
     base_url = "https://temp-mail.org/en/"
     
+    def get_mail(self) -> str:
+        while (mail := self.driver.find_element_by_id("mail").get_property("value")) in ["", "Loading ..."]:
+            pass
+        return mail
+    
     def main(self) -> None:
+        # print(self.get_mail())
+        sleep(5)
+        print(self.get_mail())
+        
         input()
